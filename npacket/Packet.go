79716ed6b2d5c0900148d8ab2a5632104fe586a7
Ethan <ethanjhem.e@gmail.com>
2021-10-13 18:01:27 +0100

    Add TODO and packet interface (will finish later)

--- npacket/Packet.go
+++ npacket/Packet.go
@@ -7,7 +7,7 @@ import (
 
 var err error
 
-var Log = logging.MustGetLogger("HoneyGO")
+var Log = logging.MustGetLogger("HoneyBEE")
 
 type GeneralPacket struct {
 	PacketSize   int32
@@ -19,11 +19,18 @@ type GeneralPacket struct {
 
 //Currently weighing if it's worth to use an interface or just use struct methods, currently just gonna use struct methods for ease
 
-// type PacketCodec interface {
-// 	Encode() *PacketWriter
-// 	Decode() //*GeneralPacket
-// 	//Create()
-// }
+type PacketCodec interface {
+	Encode() *PacketWriter
+	Decode() *PacketReader //*GeneralPacket
+}
+
+func Encode(p PacketCodec) {
+	p.Encode()
+}
+
+func Decode(p PacketCodec) {
+	p.Decode()
+}
 
 func (PW *PacketWriter) Send(Conn *gnet.Conn) {
 	return
