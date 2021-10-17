bd041afb77208bced895d93a777fc398d7c2ef8f
Ethan <ethanjhem.e@gmail.com>
2021-10-17 02:14:05 +0100

    Move start of server.go to init.go, and minor shuffles

--- npacket/Writer_test.go
+++ npacket/Writer_test.go
@@ -8,21 +8,21 @@ import (
 func TestCreatePacketWriter(T *testing.T) {
 	PW := CreatePacketWriter(0x00)
 	if cap(PW.data) != 128 {
-		T.Error(writerLength)
+		T.Error(errWriterLength)
 	}
 }
 
 func TestCreatePacketWriterWithCapacity(T *testing.T) {
 	PW := CreatePacketWriterWithCapacity(0x00, 256)
 	if cap(PW.data) != 256 {
-		T.Error(writerLength)
+		T.Error(errWriterLength)
 	}
 }
 
 func TestCreatePacketWriterWithCapacityBelow0(T *testing.T) {
 	PW := CreatePacketWriterWithCapacity(0x00, -45345)
 	if cap(PW.data) != 128 {
-		T.Error(writerLength)
+		T.Error(errWriterLength)
 	}
 }
 
@@ -30,11 +30,11 @@ func TestWriteByte(T *testing.T) {
 	PW := CreatePacketWriter(0x00)
 	PW.WriteByte(8)
 	if len(PW.data) != 2 {
-		T.Error(writerLength)
+		T.Error(errWriterLength)
 		T.Error(PW.data)
 	} else {
 		if PW.data[1] != 8 {
-			T.Error(writerValue)
+			T.Error(errWriterValue)
 		}
 	}
 }
@@ -43,10 +43,10 @@ func TestWriteBoolean(T *testing.T) {
 	PW.WriteBoolean(true)
 	PW.WriteBoolean(false)
 	if PW.data[1] != 1 {
-		T.Error(writerValue)
+		T.Error(errWriterValue)
 	}
 	if PW.data[2] != 0 {
-		T.Error(writerValue)
+		T.Error(errWriterValue)
 	}
 }
 func TestVarInt(T *testing.T) {
@@ -63,13 +63,4 @@ func TestVarInt(T *testing.T) {
 	fmt.Println("PID: ", PID, "NR: ", NR)
 	NUM, NR2, _ := PR.ReadVarInt()
 	fmt.Println("NUM: ", NUM, "NR: ", NR2)
-	// if NUM != 2000000 {
-	// 	T.Error(writerValue)
-	// }
-	// if NR != 3 {
-	// 	T.Error(writerExpectedError)
-	// }
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
